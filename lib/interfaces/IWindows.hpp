3c3ecd4ec6403156ebbf74b42bb65beae91b6f60
benjamin.peixoto <benjamin.peixoto@epitech.eu>
2017-03-22 18:04:57 +0100

    Modif interface

--- lib/interfaces/IWindows.hpp
+++ lib/interfaces/IWindows.hpp
@@ -5,33 +5,70 @@
 #ifndef ARCADE_IWINDOWS_HPP
 #define ARCADE_IWINDOWS_HPP
 
+#include <memory>
 #include "Vector3d.hpp"
 #include "IObject.hpp"
 #include "IObserve.hpp"
 
 namespace arcade
 {
+  //
+  // Enum to differentiate frame that update the game and
+  // frame only to update the display
+  //
 
-  // Basic Windows Object
+  enum class FrameType: char
+  {
+    GameFrame,
+    UpdateFrame
+  };
+
+  //
+  //		Basic Windows Object
+  //
 
   class IWindows: public IObserved
   {
    public:
-    virtual ~IWindows() {};
+    virtual ~IWindows(void) {};
 
-    virtual bool isOpen() const = 0;
-    virtual int32_t getHeight() const = 0;
-    virtual int32_t getLenght() const = 0;
-    virtual Vector3d const &getSize() const = 0;
+    //
+    //		Check if windows succesfully opened
+    //
 
-    virtual int event() = 0;
-    virtual int refresh() = 0;
+    virtual bool		isOpen(void) const = 0;
 
-    virtual void addObject(std::shared_ptr<arcade::IObject>, Vector3d const &) = 0;
-    virtual void addObject(std::shared_ptr<arcade::IObject>) = 0;
-    virtual void moveObject(std::shared_ptr<arcade::IObject>, Vector3d const &) = 0;
-    virtual void destroyObject(std::shared_ptr<arcade::IObject>) = 0;
+    //
+    //		Getters
+    //
 
+    virtual int32_t		getHeight(void) const = 0;
+    virtual int32_t		getLenght(void) const = 0;
+    virtual Vector3d const &	getSize(void) const = 0;
+
+    //
+    //		GameLoop function
+    //
+
+    virtual bool		event(void) = 0;
+    virtual arcade::FrameType	refresh(void) = 0;
+
+    //
+    //		Object gestion functions
+    //
+
+    // Create object and/not move
+
+    virtual void		addObject(std::shared_ptr<arcade::IObject>, Vector3d const &) = 0;
+    virtual void		addObject(std::shared_ptr<arcade::IObject>) = 0;
+
+    // Move object to pos
+
+    virtual void		moveObject(std::shared_ptr<arcade::IObject>, Vector3d const &) = 0;
+    virtual void		moveObject(std::string, Vector3d const &) = 0;
+
+    // Destroy object
+    virtual void		destroyObject(std::shared_ptr<arcade::IObject>) = 0;
   };
 };
 
