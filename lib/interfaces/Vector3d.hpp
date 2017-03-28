b79ab25b1434c12c3f804346f15f2be7fea6830a
Vincent Mesquita <vincent.mesquita@epitech.eu>
2017-03-28 14:01:39 +0200

    MAJ interfaces

--- lib/interfaces/Vector3d.hpp
+++ lib/interfaces/Vector3d.hpp
@@ -6,6 +6,7 @@
 #define ARCADE_VECTOR3D_HPP
 
 #include <cstdint>
+#include <ostream>
 
 namespace arcade
 {
@@ -16,56 +17,33 @@ namespace arcade
     int32_t _y;
     int32_t _z;
    public:
-    Vector3d(int32_t x, int32_t y, int32_t z = 0)
-	    : _x(x), _y(y), _z(z)
-    {};
+    Vector3d(int32_t x, int32_t y, int32_t z = 0);
 
-    Vector3d(Vector3d const &other)
-	    : _x(other.getX()), _y(other.getY()), _z(other.getZ())
-    {};
+    Vector3d(Vector3d const &other);
 
-    ~Vector3d(void)
-    {};
+    ~Vector3d(void);
 
-    Vector3d &operator=(Vector3d const &other)
-    {
-      if (&other != this)
-	{
-	  this->_x = other.getX();
-	  this->_y = other.getY();
-	  this->_z = other.getZ();
-	}
-      return (*this);
-    }
+    Vector3d &operator=(Vector3d const &other);
 
-    int32_t getX(void) const
-    { return (this->_x); };
+    int32_t getX(void) const;
 
-    int32_t getY(void) const
-    { return (this->_y); };
+    int32_t getY(void) const;
 
-    int32_t getZ(void) const
-    { return (this->_z); };
+    int32_t getZ(void) const;
 
-    void setX(int32_t x)
-    { this->_x = x; };
+    void setX(int32_t x);
 
-    void setY(int32_t y)
-    { this->_y = y; };
+    void setY(int32_t y);
 
-    void setZ(int32_t z)
-    { this->_z = z; };
+    void setZ(int32_t z);
 
-    bool operator==(Vector3d const &other)
-    {
-      return (this->getX() == other.getX() && this->getY() == other.getY());
-    }
+    bool operator==(Vector3d const &other) const;
 
-    Vector3d operator+(Vector3d const &other) const
-    {
-      return (Vector3d(this->getX() + other.getX(), this->getY() + other.getY()));
-    };
+    Vector3d operator+(Vector3d const &other) const;
   };
+
+  std::ostream		&operator<<(std::ostream &os, const Vector3d &vector);
+
 };
 
 #endif //ARCADE_VECTOR3D_HPP
