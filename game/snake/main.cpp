afd417f84f68976f3c839c2246454bacebf10534
kyxo <florian.vincent@epitech.eu>
2017-04-09 01:23:26 +0200

    Snake Terminé
    Former-commit-id: f4645927968d1bbdb773455ad3f9f02fc55b5b74

--- game/snake/main.cpp
+++ game/snake/main.cpp
@@ -1,38 +1,9 @@
-//
-// Created by vincent on 04/04/17.
-//
+  // The famous header
 
-#include <dlfcn.h>
-#include "IGraphicalLib.hpp"
-#include "IGameLib.hpp"
+extern "C" void Play(void);
 
-typedef arcade::IGameLib *(*fptr)(void*);
-
-int     main(void)
+int main(void)
 {
-    void *handle = dlopen("../lib_arcade_snake.so", RTLD_NOW | RTLD_LAZY);
-    if (!handle)
-    {
-        fputs (dlerror(), stderr);
-        return (1);
-    }
-    fptr lib_fptr = (fptr)dlsym(handle, "getNewLib");
-    if (!lib_fptr)
-    {
-        fputs (dlerror(), stderr);
-        return (1);
-    }
-
-    arcade::IGameLib *gameLib;
-    std::shared_ptr<std::vector<std::shared_ptr<arcade::IObject>>> obj;
-
-    try {
-        gameLib = lib_fptr(handle);
-        gameLib->initGame(NULL, obj);
-    } catch (const std::exception &e) {
-        std::cout << "Error: " + std::string(e.what()) << std::endl;
-    }
-    dlclose(handle);
-//    delete gameLib;
-    return (0);
+  Play();
+  return (0);
 }
