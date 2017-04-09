30ab91bb55b4998aff81ed7677384a614c24fd18
Vincent Mesquita <vincent.mesquita@epitech.eu>
2017-04-09 18:18:47 +0200

    [Core] EvenementGame
    Former-commit-id: 59671b312ea94d1a435c6b63607e3925c6b85cc7

---
+++ src/EvenementGame.hpp
@@ -0,0 +1,33 @@
+//
+// Created by vincent on 09/04/17.
+//
+
+#ifndef CPP_ARCADE_EVENEMENTGAME_HPP
+#define CPP_ARCADE_EVENEMENTGAME_HPP
+
+#include "IObserve.hpp"
+
+class EvenementGame : public IEvenement
+{
+ public:
+  EvenementGame(IEvenement::KeyCode);
+  virtual ~EvenementGame();
+
+  virtual IEvenement::Action		getAction(void) const;
+  virtual IEvenement::KeyCode		getKeyCode(void) const;
+  virtual uint64_t			getScore(void) const;
+  virtual int32_t			getData(void) const;
+
+  void 					setAction(const IEvenement::Action);
+  void 					setKeyCode(const IEvenement::KeyCode);
+  void 					setScore(const uint64_t);
+
+ private:
+  IEvenement::KeyCode 			_keyCode;
+  IEvenement::Action 			_action;
+  uint64_t 				_score;
+
+};
+
+
+#endif //CPP_ARCADE_EVENEMENTGAME_HPP
