57f1f005648f84bd9d77c792cfee7d83329ef5ed
Vincent Mesquita <vincent.mesquita@epitech.eu>
2017-04-09 13:28:36 +0200

    [Core]Main observer

---
+++ src/MainObserver.cpp
@@ -0,0 +1,11 @@
+//
+// Created by vincent on 09/04/17.
+//
+
+#include "MainObserver.hpp"
+
+arcade::MainObserver::MainObserver() { }
+
+arcade::MainObserver::~MainObserver() { }
+
+void arcade::MainObserver::getNotified(IEvenement const &) { }
