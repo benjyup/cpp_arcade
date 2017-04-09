afe775c95e8d62974a7039bf69f81760c93851cf
Vincent Mesquita <vincent.mesquita@epitech.eu>
2017-04-09 18:19:53 +0200

    [Ncurses] norme
    Former-commit-id: 1e93b683b53a5e0deb5ada0ff70388e171b5103c

--- lib/ncurses/src/Label.cpp
+++ lib/ncurses/src/Label.cpp
@@ -7,48 +7,46 @@
 
 namespace arcade
 {
-    Label::Label(const std::string &name, const std::string &filename)
-    {
-        _isMoving = false;
-        _name = name;
-        //_filename = Object::directory_name + _filename + Object::file_extension;
-        _filename = filename + file_extension;
-        setProperties(_filename);
-        _string = "";
-        _position = Vector3d(0, 0);
-        _direction = Vector3d(0, 0);
-        _speed = 0;
-        //setProperties(filename);
-    }
+  Label::Label(const std::string &name, const std::string &filename)
+  {
+    _isMoving = false;
+    _name = name;
+    _filename = filename + file_extension;
+    setProperties(_filename);
+    _string = "";
+    _position = Vector3d(0, 0);
+    _direction = Vector3d(0, 0);
+    _speed = 0;
+  }
 
-    Label::~Label() {}
+  Label::~Label() { }
 
-    bool Label::setProperties(const std::string &filename)
-    {
-        std::ifstream 		fs;
-        std::vector<std::string>	properties = NcursesTools::NT_DEFAULT_PROPERTIES;
-        std::string			str;
-        unsigned int		i = 0;
+  bool 				Label::setProperties(const std::string &filename)
+  {
+    std::ifstream 		fs;
+    std::vector<std::string>	properties = NcursesTools::NT_DEFAULT_PROPERTIES;
+    std::string			str;
+    unsigned int		i = 0;
 
-        fs.open(filename);
-        if (fs.is_open())
-        {
-            while (!fs.eof() && i < properties.size())
-            {
-                getline(fs, str);
-                if (i != 0)
-                    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-                if (!str.empty() && str != NcursesTools::NT_NONE)
-                    properties[i] = str;
-                i += 1;
-            }
-            (void)fs.close();
-        }
-        _string = properties[0];
-        _color = properties[1];
-        return (true);
-    }
+    fs.open(filename);
+    if (fs.is_open())
+      {
+	while (!fs.eof() && i < properties.size())
+	  {
+	    getline(fs, str);
+	    if (i != 0)
+	      std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	    if (!str.empty() && str != NcursesTools::NT_NONE)
+	      properties[i] = str;
+	    i += 1;
+	  }
+	fs.close();
+      }
+    _string = properties[0];
+    _color = properties[1];
+    return (true);
+  }
 
-    Object::ObjectType Label::getType() const { return (ObjectType::Label); }
+  Object::ObjectType 		Label::getType() const { return (ObjectType::Label); }
 
 }
