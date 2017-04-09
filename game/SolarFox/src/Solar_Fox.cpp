889caa000ce98231e017f569cd7fd8efb114d076
peixot_b <benjamin.peixoto@epitech.eu>
2017-04-09 13:14:41 +0200

    [SFML]Modif du throw

---
+++ game/SolarFox/src/Solar_Fox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peixot_b on 09/04/17.
+//
+
+#include "Solar_Fox.hpp"
---
+++ game/SolarFox/src/Solar_Fox.hpp
@@ -0,0 +1,14 @@
+//
+// Created by peixot_b on 09/04/17.
+//
+
+#ifndef CPP_ARCADE_SOLAR_FOX_HPP
+#define CPP_ARCADE_SOLAR_FOX_HPP
+
+
+class Solar_Fox {
+
+};
+
+
+#endif //CPP_ARCADE_SOLAR_FOX_HPP
