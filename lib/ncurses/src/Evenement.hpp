96a43f889388ab67eb5756758b7974f700f52c9b
Vincent Mesquita <vincent.mesquita@epitech.eu>
2017-04-09 18:19:53 +0200

    [Ncurses] norme

--- lib/ncurses/src/Evenement.hpp
+++ lib/ncurses/src/Evenement.hpp
@@ -9,26 +9,26 @@
 
 namespace arcade
 {
-    class Evenement : public IEvenement
-    {
-    public:
-        Evenement(IEvenement::KeyCode);
-        virtual ~Evenement();
+  class Evenement : public IEvenement
+  {
+   public:
+    Evenement(IEvenement::KeyCode);
+    virtual ~Evenement();
 
-        virtual IEvenement::Action		getAction(void) const;
-        virtual IEvenement::KeyCode		getKeyCode(void) const;
-        virtual uint64_t			getScore(void) const;
-        virtual int32_t			getData(void) const;
+    virtual IEvenement::Action		getAction(void) const;
+    virtual IEvenement::KeyCode		getKeyCode(void) const;
+    virtual uint64_t			getScore(void) const;
+    virtual int32_t			getData(void) const;
 
-        void 				setAction(const IEvenement::Action);
-        void 				setKeyCode(const IEvenement::KeyCode);
-        void 				setScore(const uint64_t);
+    void 				setAction(const IEvenement::Action);
+    void 				setKeyCode(const IEvenement::KeyCode);
+    void 				setScore(const uint64_t);
 
-    private:
-        IEvenement::KeyCode 		_keyCode;
-        IEvenement::Action 			_action;
-        uint64_t 				_score;
-    };
+   private:
+    IEvenement::KeyCode 		_keyCode;
+    IEvenement::Action 			_action;
+    uint64_t 				_score;
+  };
 }
 
 #endif //CPP_ARCADE_EVENEMENT_HPP
--- lib/ncurses/src/Label.hpp
+++ lib/ncurses/src/Label.hpp
@@ -10,16 +10,16 @@
 
 namespace arcade
 {
-    class Label : public Object
-    {
-    public:
-        Label(const std::string &name, const std::string &filename);
-        virtual ~Label();
+  class Label : public Object
+  {
+   public:
+    Label(const std::string &name, const std::string &filename);
+    virtual ~Label();
 
-        virtual bool 			setProperties(const std::string &pahtname);
-        virtual Object::ObjectType 		getType(void) const;
-    private:
-    };
+    virtual bool 			setProperties(const std::string &pahtname);
+    virtual Object::ObjectType 		getType(void) const;
+   private:
+  };
 }
 
 #endif //CPP_ARCADE_LABEL_HPP
